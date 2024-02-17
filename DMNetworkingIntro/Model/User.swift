58c45ba53b288385f49c02c639b646523aaf4b2e
Beau Enslow <beauenslow@Beaus-MacBook-Pro.local>
2024-02-16 17:15:54 -0700

    Added ReadMe

--- DMNetworkingIntro/Model/User.swift
+++ DMNetworkingIntro/Model/User.swift
@@ -4,7 +4,7 @@
 //
 //  Created by David Ruvinskiy on 4/10/23.
 /**
- 2.1 For this project, we will be working with **only the "/users" endpoint**. The JSON you get back from this endpoint will contain many keys, but we care about only the "data" key. The value for this key is an array of several JSON objects representing fake users.
+ 2.1 For this project, we will be working with **only the "/users" endpoint**. The JSON you get back from this endpoint will contain many keys, but we care about only the "data" key. The value for this key is an array of several JSON objects representing fake users endpoints: : https://reqres.in/api-docs/#/default/get_users
  Familiarize yourself with the documentation for the endpoint: https://reqres.in/api-docs/#/default/get_users. Do not move on until you until you're comfortable with the data you'll get back.
  2.2 Based on the JSON, create a struct called `User` that conforms to the `Codable` protocol and contains the following properties:
     - An Int `id`
---
+++ README.md
@@ -0,0 +1,4 @@
+## IOS API Assignment
+
+# For this project, we will be working with the "/users" endpoint. The JSON you get back from this endpoint will contain many keys, but we care about only the "data" key. The value for this key is an array of several JSON objects representing fake users endpoints: https://reqres.in/api/users
+
